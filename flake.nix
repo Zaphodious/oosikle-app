18e42a041735fe24c657256c1e9031849eb8fab4
HotFish <admin@hot.fish>
2025-03-05 19:46:43 -0500

    use make-tauri-app after making flake

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "Oosikle";
+  inputs = {
+
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      rust-overlay,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default;
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              gobject-introspection
+              cargo
+              cargo-tauri
+              bun
+              rust
+            ];
+            buildInputs = with pkgs; [
+              at-spi2-atk
+              atkmm
+              cairo
+              gdk-pixbuf
+              glib
+              gtk3
+              harfbuzz
+              librsvg
+              libsoup_3
+              pango
+              webkitgtk_4_1
+              openssl
+
+              eza
+              fd
+              pre-commit
+            ];
+            RUST_SRC_PATH = rust;
+
+            shellHook = ''
+              alias ls=eza
+              alias find=fd
+            '';
+          };
+      }
+    );
+}
