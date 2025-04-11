87da6ecb7a8fb46a316fa5f2a9d285f86a7fdb7d
HotFish <admin@hot.fish>
2025-04-10 20:06:52 -0400

    Continue ergo-izing nascant db api

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -12,8 +12,8 @@ pub fn init_db(db_loc: &str) -> Result<Connection, Error> {
     return Ok(conn);
 }
 
-pub trait DBRecord<U, T: DBRecord<U,T>> {
-    fn get_from_id(id: &U, conn: &Connection) -> Result<Option<T>, Error>;
+pub trait DBRecord<U> {
+    fn get_from_id(conn: &Connection, id: &U) -> Result<Option<Self>, Error> where Self: Sized;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -31,8 +31,8 @@ pub struct ObjectAttr {
     data: AttrValue,
 }
 
-impl DBRecord<(&Uuid, &String), ObjectAttr> for ObjectAttr {
-    fn get_from_id(id: &(&Uuid, &String), conn: &Connection) -> Result<Option<ObjectAttr>, Error> {
+impl DBRecord<(&Uuid, &str)> for ObjectAttr {
+    fn get_from_id(conn: &Connection, id: &(&Uuid, &str)) -> Result<Option<ObjectAttr>, Error> {
         let mut stmt = conn.prepare_cached( "select * from ObjectAttributes where ObjectAttributes.object_uuid = ?1 and ObjectAttributes.attribute_name = ?2;")?;
         let record: Option<ObjectAttr> = stmt
             .query_row(*id, ObjectAttr::from_row)
@@ -75,8 +75,8 @@ pub struct ObjectRecord {
     pub media_type_override: Option<String>
 }
 
-impl DBRecord<Uuid, ObjectRecord> for ObjectRecord {
-    fn get_from_id(id: &Uuid, conn: &Connection) -> Result<Option<ObjectRecord>, Error> {
+impl DBRecord<Uuid> for ObjectRecord {
+    fn get_from_id(conn: &Connection, id: &Uuid)  -> Result<Option<ObjectRecord>, Error> {
         let mut stmt = conn.prepare_cached( "select * from ObjectRecordView where ObjectRecordView.uuid = ?1;")?;
         let record = stmt
             .query_row([id], ObjectRecord::from_view)
@@ -116,7 +116,6 @@ pub struct FileDescription {
     pub media_type_override: Option<String>,
     pub deleted: bool,
     pub read_only: bool
-
 }
 
 impl FileDescription {
@@ -142,37 +141,22 @@ pub struct CollectionRecord {
     objects: Vec<ObjectRecord>,
 }
 
-pub fn get_object_by_uuid(
-    conn: &Connection,
-    the_uuid: &Uuid,
-) -> Result<Option<ObjectRecord>, Error> {
-    let mut stmt = conn.prepare_cached( "select * from ObjectRecordView where ObjectRecordView.uuid = ?1;")?;
-    /*
-     *  ##deserializing using query() and from_rows(), the most efficient way
-        let mut statement = connection.prepare("SELECT * FROM example").unwrap();
-        let mut res = from_rows::<Example>(statement.query([]).unwrap());
-        assert_eq!(res.next().unwrap().unwrap(), row1);
-        assert_eq!(res.next().unwrap().unwrap(), Example { id: 2, name: "second name".into() });
-     */
-    let record = stmt
-        .query_row([the_uuid], ObjectRecord::from_view)
-        .optional()?;
-    return Ok(record);
+impl DBRecord<Uuid> for CollectionRecord {
+    fn get_from_id(conn: &Connection, id: &Uuid) -> Result<Option<CollectionRecord>, Error> {
+        let mut coll_stmt = conn.prepare_cached("select * from Collections where Collections.collection_uuid = ?1;")?;
+        let mut obj_stmt = conn.prepare_cached("select * from ObjectRecordView left join ObjectsInCollections on ObjectsInCollections.object_uuid = ObjectRecordView.uuid where ObjectsInCollections.collection_uuid = ?1;")?;
+        let coll_rec = coll_stmt.query_row([id], |row| {
+            let objects = obj_stmt.query_map([id], ObjectRecord::from_view)?;
+            Ok(CollectionRecord {
+                uuid: row.get("collection_uuid")?,
+                name: row.get("collection_name")?,
+                objects: objects.map(|t|t.expect("for now")).collect()
+            })
+        }).optional()?;
+        return Ok(coll_rec);
+    }
 }
 
-pub fn get_collection_by_uuid(conn: &Connection, the_uuid: &Uuid) -> Result<Option<CollectionRecord>> {
-    let mut coll_stmt = conn.prepare_cached("select * from Collections where Collections.collection_uuid = ?1;")?;
-    let mut obj_stmt = conn.prepare_cached("select * from ObjectRecordView left join ObjectsInCollections on ObjectsInCollections.object_uuid = ObjectRecordView.uuid where ObjectsInCollections.collection_uuid = ?1;")?;
-    let coll_rec = coll_stmt.query_row([the_uuid], |row| {
-        let objects = obj_stmt.query_map([the_uuid], ObjectRecord::from_view)?;
-        Ok(CollectionRecord {
-            uuid: row.get("collection_uuid")?,
-            name: row.get("collection_name")?,
-            objects: objects.map(|t|t.expect("for now")).collect()
-        })
-    }).optional()?;
-    return Ok(coll_rec);
-}
 
 /*
 pub fn get_file_by_uuid(conn: &Connection, the_uuid: &Uuid) -> Result<Option<FileDescription>> {
@@ -197,7 +181,7 @@ mod tests {
     #[test]
     fn gets_an_object_by_uuid() -> Result<(), Error> {
         let conn = init()?;
-        let obj = ObjectRecord::get_from_id(&uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"), &conn)?
+        let obj = ObjectRecord::get_from_id(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?
             .expect("There is no entity here");
         assert!(obj.name == "Welcome File");
         return Ok(());
@@ -206,7 +190,7 @@ mod tests {
     #[test]
     fn doesnt_get_an_object_that_doesnt_exist() -> Result<(), Error> {
         let conn = init()?;
-        let no_obj = get_object_by_uuid(&conn, &uuid!("ABADCAFEABADCAFEABADCAFEABADCAF1"))?;
+        let no_obj = ObjectRecord::get_from_id(&conn, &uuid!("ABADCAFEABADCAFEABADCAFEABADCAF1"))?;
         if no_obj.is_some() {
             assert!(false, "There should not be an entity with this fake UUID")
         };
@@ -216,7 +200,7 @@ mod tests {
     #[test]
     fn gets_attributes_for_an_object() -> Result<(), Error> {
         let conn = init()?;
-        let attrs = ObjectRecord::get_from_id(&uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"), &conn)?.expect("There should be an object here").get_attributes(&conn)?;
+        let attrs = ObjectRecord::get_from_id(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?.expect("There should be an object here").get_attributes(&conn)?;
         //let attrs = get_attributes_for_object(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?;
         assert!(attrs[0].name.len() != 0);
         assert!(attrs[1].name.len() != 0);
@@ -228,7 +212,7 @@ mod tests {
     #[test]
     fn gets_a_spcific_attribute_for_an_object() -> Result<(), Error> {
         let conn = init()?;
-        let attr = ObjectAttr::get_from_id(&(&uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"), &"author".to_string()), &conn)?.expect("There should be an attribute here");
+        let attr = ObjectAttr::get_from_id(&conn, &(&uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"), "author"))?.expect("There should be an attribute here");
         if let AttrValue::STRING(_) = attr.data {} else {
             assert!(false);
         }
@@ -238,7 +222,7 @@ mod tests {
     #[test]
     fn gets_a_collection_by_uuid() -> Result<(), Error> {
         let conn = init()?;
-        let coll = get_collection_by_uuid(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
+        let coll = CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
             .expect("There is no collection here");
         assert!(coll.name == "Default Briefcase");
         return Ok(());
@@ -247,7 +231,7 @@ mod tests {
     #[test]
     fn getting_a_collection_by_uuid_gets_objects_in_it() -> Result<(), Error> {
         let conn = init()?;
-        let coll = get_collection_by_uuid(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
+        let coll = CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
             .expect("There is no collection here");
         assert!(coll.objects[0].name == "Welcome File");
         return Ok(());
