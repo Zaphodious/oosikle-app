89c9df527f1ed468342c7c549d3f68aa02ce7e5b
Alexander Chythlook <achythlook@gmail.com>
2025-04-11 16:05:05 -0400

    Take boilerplate out and stick it in a default trait implimentation

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,10 @@
 use micromap::Map;
-use rusqlite::{types::ValueRef, Connection, Error, OptionalExtension, Result, Row};
+use rusqlite::{
+    params, types::ValueRef, CachedStatement, Connection, Error, OptionalExtension, Params, Result,
+    Row, ToSql,
+};
 use serde::{Deserialize, Serialize};
+use serde_rusqlite as sr;
 use std::vec::Vec;
 use uuid::{uuid, Uuid};
 
@@ -12,8 +16,22 @@ pub fn init_db(db_loc: &str) -> Result<Connection, Error> {
     return Ok(conn);
 }
 
-pub trait DBFlatRecord<U> {
-    fn get_from_id(conn: &Connection, id: &U) -> Result<Option<Self>, Error> where Self: Sized;
+pub trait DBSimpleRecord {
+    fn from_row(row: &Row) -> Result<Self, Error>
+    where
+        Self: Sized;
+}
+
+pub trait DBQuickGettable<U: ToSql>: DBSimpleRecord {
+    fn get_fetch_sql() -> &'static str;
+    fn get_from_id(conn: &Connection, id: &U) -> Result<Option<Self>, Error>
+    where
+        Self: Sized,
+    {
+        let mut stmt = conn.prepare_cached(Self::get_fetch_sql())?;
+        let record = stmt.query_row([id], Self::from_row).optional()?;
+        return Ok(record);
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -31,6 +49,26 @@ pub struct ObjectAttr {
     data: AttrValue,
 }
 
+impl DBSimpleRecord for ObjectAttr {
+    fn from_row(row: &Row) -> Result<ObjectAttr, Error> {
+        Ok(ObjectAttr {
+            name: row.get("attribute_name")?,
+            data: match row.get_ref("attribute_value")? {
+                ValueRef::Null => AttrValue::NONE,
+                ValueRef::Integer(i) => AttrValue::INT(i),
+                ValueRef::Real(f) => AttrValue::FLOAT(f),
+                ValueRef::Text(s) => AttrValue::STRING(
+                    String::from_utf8(s.to_vec()).expect("A text string was not utf-8"),
+                ),
+                ValueRef::Blob(b) => {
+                    AttrValue::STRING(String::from_utf8(b.to_vec()).expect("A blob went wrong idk"))
+                }
+            },
+        })
+    }
+}
+
+/*
 impl DBFlatRecord<(&Uuid, &str)> for ObjectAttr {
     fn get_from_id(conn: &Connection, id: &(&Uuid, &str)) -> Result<Option<ObjectAttr>, Error> {
         let mut stmt = conn.prepare_cached( "select * from ObjectAttributes where ObjectAttributes.object_uuid = ?1 and ObjectAttributes.attribute_name = ?2;")?;
@@ -39,7 +77,10 @@ impl DBFlatRecord<(&Uuid, &str)> for ObjectAttr {
             .optional()?;
         return Ok(record);
     }
-}
+    fn get_fetch_sql() -> &'static str {
+         "select * from ObjectAttributes where ObjectAttributes.object_uuid = ?1 and ObjectAttributes.attribute_name = ?2;"
+    }
+}*/
 /*
 create table ObjectAttributes (
     object_uuid blob not null,
@@ -50,20 +91,7 @@ create table ObjectAttributes (
 );
  */
 
-impl ObjectAttr {
-    fn from_row(row: &Row) -> Result<ObjectAttr, Error> {
-        Ok(ObjectAttr {
-            name: row.get("attribute_name")?,
-            data: match row.get_ref("attribute_value")? {
-                ValueRef::Null => AttrValue::NONE,
-                ValueRef::Integer(i) => AttrValue::INT(i),
-                ValueRef::Real(f) => AttrValue::FLOAT(f),
-                ValueRef::Text(s) => AttrValue::STRING(String::from_utf8(s.to_vec()).expect("A text string was not utf-8")),
-                ValueRef::Blob(b) => AttrValue::STRING(String::from_utf8(b.to_vec()).expect("A blob went wrong idk"))
-            },
-        })
-    }
-}
+impl ObjectAttr {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ObjectRecord {
@@ -72,35 +100,48 @@ pub struct ObjectRecord {
     pub manager: String,
     pub file_path: String,
     pub deleted: bool,
-    pub media_type_override: Option<String>
+    pub media_type_override: Option<String>,
 }
-
-impl DBFlatRecord<Uuid> for ObjectRecord {
-    fn get_from_id(conn: &Connection, id: &Uuid)  -> Result<Option<ObjectRecord>, Error> {
-        let mut stmt = conn.prepare_cached( "select * from ObjectRecordView where ObjectRecordView.uuid = ?1;")?;
-        let record = stmt
-            .query_row([id], ObjectRecord::from_view)
-            .optional()?;
-        return Ok(record);
-    }
-}
-
-impl ObjectRecord {
-    fn from_view(row: &Row) -> Result<ObjectRecord, Error> {
-        Ok( ObjectRecord {
+impl DBSimpleRecord for ObjectRecord {
+    fn from_row(row: &Row) -> Result<ObjectRecord, Error> {
+        Ok(ObjectRecord {
             uuid: row.get("uuid")?,
             name: row.get("name")?,
             manager: row.get("manager")?,
             file_path: row.get::<&str, String>("file_path")?,
             deleted: row.get("deleted")?,
-            media_type_override: row.get("media_type_override")?
+            media_type_override: row.get("media_type_override")?,
         })
     }
+}
+impl DBQuickGettable<Uuid> for ObjectRecord {
+    /*
+    fn get_from_id(conn: &Connection, id: &Uuid)  -> Result<Option<ObjectRecord>, Error> {
+        let mut stmt =
+        let record = stmt
+            .query_row([id], ObjectRecord::from_view)
+            .optional()?;
+        return Ok(record);
+    }*/
+    fn get_fetch_sql() -> &'static str {
+        "select * from ObjectRecordView where ObjectRecordView.uuid = ?1;"
+    }
+}
 
-    fn get_attributes (&self, conn: &Connection) -> Result<Vec<ObjectAttr>> {
-        let mut stmt = conn.prepare_cached("select * from ObjectAttributes where ObjectAttributes.object_uuid = ?")?;
+impl ObjectRecord {
+    fn get_attributes(&self, conn: &Connection) -> Result<Vec<ObjectAttr>> {
+        let mut stmt = conn.prepare_cached(
+            "select * from ObjectAttributes where ObjectAttributes.object_uuid = ?",
+        )?;
         let attr_rows = stmt.query_map([&self.uuid], ObjectAttr::from_row)?;
-        Ok(attr_rows.map(|t|t.expect("just for now")).collect())
+        Ok(attr_rows.map(|t| t.expect("just for now")).collect())
+    }
+    fn get_attribute(&self, conn: &Connection, name: &str) -> Result<Option<ObjectAttr>> {
+        let mut stmt = conn.prepare_cached("select * from ObjectAttributes where ObjectAttributes.object_uuid = ?1 and ObjectAttributes.attribute_name = ?2")?;
+        let record = stmt
+            .query_row(params![self.uuid, name], ObjectAttr::from_row)
+            .optional()?;
+        return Ok(record);
     }
 }
 
@@ -115,35 +156,47 @@ pub struct FileDescription {
     pub encoding: String,
     pub media_type_override: Option<String>,
     pub deleted: bool,
-    pub read_only: bool
+    pub read_only: bool,
 }
 
-impl FileDescription {
-}
+impl FileDescription {}
 /*
-    file_uuid blob primary key,
-    file_name text not null,
-    file_size_bytes integer not null,
-    file_hash text not null,
-    file_path text not null,
-    file_extension_tag text not null,
-    file_encoding text,
-    media_type_override_id text,
-    file_deleted integer,
-    read_only integer,
- */
-
+   file_uuid blob primary key,
+   file_name text not null,
+   file_size_bytes integer not null,
+   file_hash text not null,
+   file_path text not null,
+   file_extension_tag text not null,
+   file_encoding text,
+   media_type_override_id text,
+   file_deleted integer,
+   read_only integer,
+*/
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CollectionRecord {
     uuid: Uuid,
     name: String,
-    objects: Vec<ObjectRecord>,
 }
 
-impl DBFlatRecord<Uuid> for CollectionRecord {
+impl DBSimpleRecord for CollectionRecord {
+    fn from_row(row: &Row) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        Ok(CollectionRecord {
+            uuid: row.get("collection_uuid")?,
+            name: row.get("collection_name")?,
+        })
+    }
+}
+impl DBQuickGettable<Uuid> for CollectionRecord {
+    fn get_fetch_sql() -> &'static str {
+        "select * from Collections where Collections.collection_uuid = ?1;"
+    }
+    /*
     fn get_from_id(conn: &Connection, id: &Uuid) -> Result<Option<CollectionRecord>, Error> {
-        let mut coll_stmt = conn.prepare_cached("select * from Collections where Collections.collection_uuid = ?1;")?;
+        let mut coll_stmt = conn.prepare_cached()?;
         let mut obj_stmt = conn.prepare_cached("select * from ObjectRecordView left join ObjectsInCollections on ObjectsInCollections.object_uuid = ObjectRecordView.uuid where ObjectsInCollections.collection_uuid = ?1;")?;
         let coll_rec = coll_stmt.query_row([id], |row| {
             let objects = obj_stmt.query_map([id], ObjectRecord::from_view)?;
@@ -154,9 +207,8 @@ impl DBFlatRecord<Uuid> for CollectionRecord {
             })
         }).optional()?;
         return Ok(coll_rec);
-    }
+    }*/
 }
-
 
 /*
 pub fn get_file_by_uuid(conn: &Connection, the_uuid: &Uuid) -> Result<Option<FileDescription>> {
@@ -200,7 +252,9 @@ mod tests {
     #[test]
     fn gets_attributes_for_an_object() -> Result<(), Error> {
         let conn = init()?;
-        let attrs = ObjectRecord::get_from_id(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?.expect("There should be an object here").get_attributes(&conn)?;
+        let attrs = ObjectRecord::get_from_id(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?
+            .expect("There should be an object here")
+            .get_attributes(&conn)?;
         //let attrs = get_attributes_for_object(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?;
         assert!(attrs[0].name.len() != 0);
         assert!(attrs[1].name.len() != 0);
@@ -212,8 +266,12 @@ mod tests {
     #[test]
     fn gets_a_spcific_attribute_for_an_object() -> Result<(), Error> {
         let conn = init()?;
-        let attr = ObjectAttr::get_from_id(&conn, &(&uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"), "author"))?.expect("There should be an attribute here");
-        if let AttrValue::STRING(_) = attr.data {} else {
+        let attr = ObjectRecord::get_from_id(&conn, &uuid!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF"))?
+            .expect("There should be an object here")
+            .get_attribute(&conn, "author")?
+            .expect("There should be an attribute here");
+        if let AttrValue::STRING(_) = attr.data {
+        } else {
             assert!(false);
         }
         return Ok(());
@@ -222,19 +280,21 @@ mod tests {
     #[test]
     fn gets_a_collection_by_uuid() -> Result<(), Error> {
         let conn = init()?;
-        let coll = CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
-            .expect("There is no collection here");
+        let coll =
+            CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
+                .expect("There is no collection here");
         assert!(coll.name == "Default Briefcase");
         return Ok(());
     }
 
+    /*
     #[test]
     fn getting_a_collection_by_uuid_gets_objects_in_it() -> Result<(), Error> {
         let conn = init()?;
-        let coll = CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
-            .expect("There is no collection here");
+        let coll =
+            CollectionRecord::get_from_id(&conn, &uuid!("BADC0FFEE0DDF00DBADC0FFEE0DDF00D"))?
+                .expect("There is no collection here");
         assert!(coll.objects[0].name == "Welcome File");
         return Ok(());
-    }
-
+    } */
 }
