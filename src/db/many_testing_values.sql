3096488d5350ed3b4bbafd82632fdc67495506c4
HotFish <admin@hot.fish>
2025-04-13 17:01:26 -0400

    Begin adding additional testing values

---
+++ src/db/many_testing_values.sql
@@ -0,0 +1,142 @@
+insert into MediaCategories values ('DOCUMENT', 'Document');
+insert into MediaTypes values ('PLAINTEXT', 'Plain Text File', 'DOCUMENT');
+insert into MediaTypes values ('EBOOK', 'Electronic Book file', 'DOCUMENT');
+
+insert into MediaCategories values ('IMAGE', 'Image');
+insert into MediaTypes values ('RASTER', 'Raster Image File', 'IMAGE');
+insert into MediaTypes values ('VECTOR', 'Vector Image File', 'IMAGE');
+
+insert into MediaCategories values ('AUDIO', 'Video');
+insert into MediaTypes values ('MUSIC', 'Music File', 'AUDIO');
+insert into MediaTypes values ('AUDIOBOOK', 'Audio book file', 'AUDIO');
+
+insert into FileExtensions values ('TXT', 'Ordinary text file');
+insert into MediaTypesForFileExtensions values ('TXT', 'PLAINTEXT');
+insert into MediaTypesForFileExtensions values ('TXT', 'EBOOK');
+
+insert into FileExtensions values ('MD', 'Markdown text file');
+insert into MediaTypesForFileExtensions values ('MD', 'PLAINTEXT');
+insert into MediaTypesForFileExtensions values ('MD', 'EBOOK');
+
+insert into FileExtensions values ('EPUB', 'Epub book file');
+insert into MediaTypesForFileExtensions values ('EPUB', 'EBOOK');
+
+insert into FileExtensions values ('SVG', 'Scalable Vector Graphics file');
+insert into MediaTypesForFileExtensions values ('SVG', 'PLAINTEXT');
+insert into MediaTypesForFileExtensions values ('SVG', 'VECTOR');
+
+insert into FileExtensions values ('PNG', 'Portable Network Graphics file');
+insert into MediaTypesForFileExtensions values ('PNG', 'RASTER');
+
+insert into FileExtensions values ('JPEG', 'JPEG Image File');
+insert into MediaTypesForFileExtensions values ('JPEG', 'RASTER');
+insert into FileExtensions values ('JPG', 'JPEG Image File');
+insert into MediaTypesForFileExtensions values ('JPG', 'RASTER');
+
+
+insert into Files values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    "welcome.txt",
+    0,
+    'a38bcfa83e52932d49e36146950db40423daeb89a2e1f1b9734401bc98f1c79e',
+    '',
+    'TXT',
+    'UTF-8',
+    NULL,
+    FALSE,
+    FALSE
+);
+
+insert into Files values (
+    X'DEADBEEFDEADBEEF0000000000000000',
+    "something.png",
+    0,
+    '',
+    '',
+    'PNG',
+    'BIN',
+    NULL,
+    FALSE,
+    FALSE
+);
+
+insert into FileBlobs values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'Welcome to Oosikle!'
+);
+
+insert into Plugins values (
+    'oosikle.manager.text',
+    'Default Text File Manager',
+    1
+);
+
+insert into MediaTypesForPlugins values (
+    'oosikle.manager.text',
+    'PLAINTEXT'
+);
+
+insert into Objects values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'Welcome File',
+    'oosikle.manager.text',
+    FALSE
+);
+
+insert into ObjectAttributes values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'author',
+    'TheHotFish'
+);
+
+insert into ObjectAttributes values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'revision',
+    4
+);
+
+insert into ObjectAttributes values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'rating',
+    3.9
+);
+
+insert into ObjectAttributes values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    'editable',
+    TRUE
+);
+
+insert into Collections values (
+    X'BADC0FFEE0DDF00DBADC0FFEE0DDF00D',
+    'Default Briefcase',
+    TRUE,
+    '',
+    FALSE
+);
+
+insert into MediaCategoriesForCollections values (
+    X'BADC0FFEE0DDF00DBADC0FFEE0DDF00D',
+    'DOCUMENT'
+);
+
+insert into ObjectsInCollections values (
+    X'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+    X'BADC0FFEE0DDF00DBADC0FFEE0DDF00D',
+    0
+);
+
+insert into Devices values (
+    X'0DE2C3400DE2C3400DE2C3400DE2C340',
+    'Example Flash Drive',
+    'This is a flash drive that is used to test the program',
+    NULL
+);
+
+insert into DeviceSyncLists values (
+    X'0DE2C3400DE2C3400DE2C3400DE2C340',
+    X'BADC0FFEE0DDF00DBADC0FFEE0DDF00D',
+    'oosikle.manager.text',
+    'documents',
+    0
+);
