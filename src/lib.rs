f1105555f4f4babe8ff979e6b38a54792bb49fba
Alexander Chythlook <achythlook@gmail.com>
2025-04-18 13:57:21 -0400

    Move towards multi-window in Tauri

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,20 @@ fn retti() -> String {
     "<div>This should have replaced the thingy</div>".to_string()
 }
 
+#[tauri::command]
+async fn window_open_test(app: tauri::AppHandle) -> String {
+    let window = tauri::window::WindowBuilder::from_config(&app, &app.config().app.windows.get(1).unwrap().clone())
+        .unwrap().build().unwrap();
+    "<div>Did a new window open?</div>".to_string()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let t = tauri::Builder::default()
         //.plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![retti])
+        .invoke_handler(tauri::generate_handler![window_open_test])
         ;
     print!("We made it past creation");
     t.run(tauri::generate_context!())
