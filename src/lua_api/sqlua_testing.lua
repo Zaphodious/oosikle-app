1adac8b160f2b6a25888ad618fea3c9531e54807
HotFish <admin@hot.fish>
2025-04-25 19:15:45 -0400

    Clean up lua just a bit

--- src/lua_api/sqlua_testing.lua
+++ src/lua_api/sqlua_testing.lua
@@ -7,20 +7,6 @@ function SQLuaFetches()
     return SQL:query([[select * from Objects where Objects.object_uuid='DEADBEEFDEADBEEFDEADBEEFDEADBEEF';]], {})[1].object_name
 end
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
-
 function Foo()
     print("hello")
 end
