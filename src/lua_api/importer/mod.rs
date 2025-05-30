bef06cd651bf5ee258737437e04a20165d0b7529
Alexander Chythlook <achythlook@gmail.com>
2025-05-30 16:15:21 -0400

    Get the importer taking in a vector of paths and ending up with a root and relative paths

--- src/lua_api/importer/mod.rs
+++ src/lua_api/importer/mod.rs
@@ -1,7 +1,14 @@
 use anyhow::Result;
-use relative_path::RelativePathBuf;
-use std::path::{Path, PathBuf};
+use hypertext::html_elements::div;
+use mlua::serde::de;
+use relative_path::{Component as rComponent, PathExt, RelativePathBuf};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashSet,
+    path::{Component as sComponent, Path, PathBuf},
+};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ShippingManifest {
     pub root_dir: PathBuf,
     pub files: Vec<RelativePathBuf>,
@@ -15,19 +22,56 @@ impl ShippingManifest {
         }
     }
 
-    pub fn add_file(mut self, file: RelativePathBuf) -> Self {
+    pub fn add_relative_file(mut self, file: RelativePathBuf) -> Self {
         self.files.push(file);
         self
     }
 
-    pub fn add_files(mut self, files: &mut Vec<RelativePathBuf>) -> Self {
+    pub fn add_relative_files(mut self, files: &mut Vec<RelativePathBuf>) -> Self {
         self.files.append(files);
         self
     }
 
-    pub fn adjust_root_considering_files(mut self) -> Result<Self> {
-        Ok(self)
+    pub fn create_manifest_from_path_vec(pathvec: Vec<PathBuf>) -> Result<Self> {
+        let denatured_paths: Vec<Vec<sComponent>> =
+            pathvec.iter().map(|p| p.components().collect()).collect();
+        println!("denatured paths are {:?}", denatured_paths);
+        let mut root_accumulator = PathBuf::new();
+        let mut depth: usize = 0;
+        let mut has_found = false;
+        loop {
+            let mut last_comp: Option<&sComponent> = None;
+            for thepath in &denatured_paths {
+                let current_comp = thepath.get(depth);
+                if current_comp == None {
+                    has_found = true;
+                    break;
+                } else if last_comp == None {
+                    last_comp = current_comp;
+                } else if last_comp != current_comp {
+                    println!("components are last_comp:{:?} and current_comp:{:?}", last_comp, current_comp);
+                    has_found = true;
+                    break;
+                }
+            }
+            if has_found {
+                break;
+            } else {
+                depth += 1;
+                println!("Pushing {:?} to the root accumulator", last_comp);
+                root_accumulator.push(last_comp.unwrap());
+            }
+        }
+        println!("Root accumulator is {:?}", root_accumulator);
+        Ok(Self {
+            files: pathvec
+                .iter()
+                .map(|p| p.relative_to(&root_accumulator).unwrap())
+                .collect(),
+            root_dir: root_accumulator,
+        })
     }
+
 }
 
 #[cfg(test)]
@@ -35,15 +79,18 @@ mod file_import_tests {
     use super::*;
 
     #[test]
-    fn tests_that_root_adjustment_works() -> Result<()> {
-        let manifest = ShippingManifest::new(PathBuf::from("")).add_files(&mut vec![
-            RelativePathBuf::from("/media/videogame/pico8/thing1.png"),
-            RelativePathBuf::from("/media/videogame/pico8/thing2.png"),
-            RelativePathBuf::from("/media/videogame/pico8/thing3.png"),
-            RelativePathBuf::from("/media/videogame/mastersystem/thing1.png"),
-            RelativePathBuf::from("/media/audio/music/blind_and_frozen.mp3"),
-        ]).adjust_root_considering_files()?;
-        assert!(manifest.root_dir == PathBuf::from("/media"));
+    fn tests_making_manifest_from_path_vec() -> Result<()> {
+        let pathvec = vec![
+            PathBuf::from("c:\\media\\videogame\\pico8\\thing1.png"),
+            PathBuf::from("c:\\media\\videogame\\pico8\\thing1.png"),
+            PathBuf::from("c:\\media\\videogame\\pico8\\thing2.png"),
+            PathBuf::from("c:\\media\\videogame\\pico8\\thing3.png"),
+            PathBuf::from("c:\\media\\videogame\\mastersystem\\thing1.png"),
+            //PathBuf::from("c:\\media\\audio\\music\\blind_and_frozen.mp3"),
+        ];
+        let manifest = ShippingManifest::create_manifest_from_path_vec(pathvec)?;
+        println!("manifest is {:?}", manifest);
+        assert!(manifest.root_dir == PathBuf::from("C:\\media\\videogame"));
         Ok(())
     }
 }
