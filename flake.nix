bd94cc8149b3b8923f0f9851253cbf0d01b82f89
HotFish <admin@hot.fish>
2025-06-12 17:05:58 -0400

    update nix flake

--- flake.nix
+++ flake.nix
@@ -74,9 +74,10 @@
               xorg.libXi
               xorg.libX11
             ];
+
             RUST_SRC_PATH = rust;
 
-            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+            #LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
 
             shellHook = ''
               export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS;
