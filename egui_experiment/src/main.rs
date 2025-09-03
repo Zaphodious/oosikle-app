1ef505612f0665465800aaf20735f97dedfc2d6f
Zaph <achythlook@gmail.com>
2025-09-03 12:15:47 -0400

    Play around with a little bit of egui

---
+++ egui_experiment/src/main.rs
@@ -0,0 +1,48 @@
+use eframe::egui; // Import necessary parts of eframe and egui
+
+// The main function where our program starts
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "egui Demo",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
+
+
+// This struct holds the data (state) for our application.
+#[derive(Default)]
+struct MyApp {
+    label: String,
+    value: f32,
+}
+
+// We implement the `eframe::App` trait for our struct.
+impl eframe::App for MyApp {
+    // The `update` function is called repeatedly, once per frame.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("bingus").show(ctx, |ui| {
+            ui.heading("fooooo");
+        });
+        egui::SidePanel::left("bongus").show(ctx, |ui| {
+            ui.heading("bar");
+        });
+        egui::TopBottomPanel::top("kringus").show(ctx, |ui| {
+            ui.heading("zip")
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut self.label);
+            });
+            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                self.value += 1.0;
+            }
+            ui.label(format!("Hello '{}', value: {}", self.label, self.value));
+        });
+    }
+}
+
